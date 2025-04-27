09436d60be06261f6e0cc03e65e6108b245bb9d0
ayushjain1306 <ayushtarunajain@gmail.com>
2025-04-27 22:33:01 +0530

    Commiting URL changes for frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-mu-gules.vercel.app",
     credentials: true
 }
 
